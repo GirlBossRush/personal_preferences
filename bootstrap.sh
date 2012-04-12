636b4cd61295bf736d4511f7b3bbd6191da445a1
Eric Wright <eric@maetel>
2012-04-12 19:02:53 -0400

    Flesh out bootstrap.

---
+++ bootstrap.sh
@@ -0,0 +1,67 @@
+user=eric
+name="Eric Wright"
+email="dev@ericwright.me"
+release=lisa
+sudo sh -c 'echo "deb http://deb.opera.com/opera $release non-free" >> /etc/apt/sources.list'
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get -y zsh htop zip unzip vim cowsay adobe-flashplugin screen wget curl git build-essentials openjdk-6-jre-headless openjdk-6-jre chromium-browser vlc
+
+cd /tmp
+wget -O sublime_text_2.tar.bz2 http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202%20Build%202181%20x64.tar.bz2
+
+cd /opt
+sudo tar -jxvf /tmp/sublime_text_2.tar.bz2
+sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
+
+
+cd /home/$user
+su $user -c 'curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh'
+su $user -c 'echo alias railroad="bundle exec rails s" >> /home/$user/.zshrc'
+
+
+
+curl -L get.rvm.io | bash -s stable
+
+
+# Rename home directories to lowercase.
+cd /home/$user
+mv Pictures pictures
+mv Music music
+mv Videos videos
+mv Documents documents
+mv Desktop desktop
+mv Downloads downloads
+
+# SSH
+su $user -c 'yes | ssh-keygen -t rsa'
+
+# Git
+git config --global user.name "$name"
+git config --global user.email"$email"
+
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+git config --global color.ui auto
+
+# Dropbox
+cd /tmp
+cd ~ && wget -O - "http://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+
+## WIP
+git clone git://gist.github.com/2371581.git
+mv 2371581/dropbox /etc/init.d/
+
+sudo chmod +x /etc/init.d/dropbox
+sudo update-rc.d dropbox defaults
+
+# Vim
+echo '4' | sudo update-alternatives --config editor
+
